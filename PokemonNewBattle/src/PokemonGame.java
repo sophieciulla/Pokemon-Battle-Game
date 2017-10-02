2de55661e44443e41d4ba0e1f6dfb4299b0a3ae1
jeffrey <jeffrey@10.0.0.151>
2017-10-01 21:53:53 -0600

    new

--- PokemonNewBattle/src/PokemonGame.java
+++ PokemonNewBattle/src/PokemonGame.java
@@ -7,6 +7,7 @@ public class PokemonGame
 	{
 		static ArrayList<Pokemon> myPokemon = new ArrayList<Pokemon>();
 		static int powerOfSkill = 0;
+		static int powerOfSkillE = 0;
 		public static void main(String[] args)
 			{
 				
@@ -138,44 +139,125 @@ public class PokemonGame
 						System.out.println();
 						System.out.println(PokemonVersion.dictionary.get(randomNumber1).getName());
 						System.out.println("level: " + wildPokemonLevel);
-						int wildPokemonHP = myPokemon.get(0).getHealthPoint()-40;
+					    int wildPokemonHP = myPokemon.get(0).getHealthPoint()-40;
 						System.out.println("HP: " + wildPokemonHP);
 						String space2 = userinput.nextLine();
+						
 						System.out.println("which skills you want to choose?");
 						System.out.println("1. " + myPokemon.get(0).getSkill1() + "  2. " + myPokemon.get(0).getSkill2() + "  3. " + myPokemon.get(0).getSkill3() + "  4. " + myPokemon.get(0).getSkill4());
 						
-						userChooseSkill();
-						
-						
-						int differenceOfAttAndDef = myPokemon.get(0).getAttackPoint() - PokemonVersion.dictionary.get(randomNumber1).getDefencePoint() - 4;
-						int enemyHealthPoint = PokemonVersion.dictionary.get(randomNumber1).getHealthPoint();
-						int randomNumberAttack = (int)(Math.random()*10)+1;
-						switch(randomNumberAttack)
+						int wildPokemonAttack = myPokemon.get(0).getAttackPoint()-10;
+						int wildPokemonDefence = myPokemon.get(0).getDefencePoint()-4;
+						int differenceOfAttAndDef = myPokemon.get(0).getAttackPoint() / wildPokemonDefence;
+						int differenceOfDefAndAtt = wildPokemonAttack / myPokemon.get(0).getDefencePoint();
+						int myPokemonHP = myPokemon.get(0).getHealthPoint();
+						int totalDamageToEnemy = 0;
+						int totalDamageToPlayer = 0;
+						while(myPokemonHP>0&&wildPokemonHP>0)
 						{
-							case 1:
+							userChooseSkill();
+							enemyRandomChooseSkill();
+							int randomNumberAttack = (int)(Math.random()*10)+1;
+							
+								switch(randomNumberAttack)
 								{
-									
-									
-								}
-							case 2:
-							case 3:
-							case 4:
-							case 5:
-							case 6:
-							case 7:
-							case 8:
-							case 9:
-								{
-									
-								}
-							case 10:
-								{
-									
+									case 1:
+										{
+
+											totalDamageToEnemy = 0;
+											wildPokemonHP = wildPokemonHP - totalDamageToEnemy;
+											System.out.println("OH no, you miss, so you made the " + totalDamageToEnemy + " damage");
+											System.out.println(PokemonVersion.dictionary.get(randomNumber1).getName() + " has " + wildPokemonHP + " HP left");
+											System.out.println();
+											
+										}
+									case 2:
+									case 3:
+									case 4:
+									case 5:
+									case 6:
+									case 7:
+									case 8:
+									case 9:
+										{
+											
+											totalDamageToEnemy = differenceOfAttAndDef * powerOfSkill / 10;
+											wildPokemonHP = wildPokemonHP - totalDamageToEnemy;
+											System.out.println("you made the " + totalDamageToEnemy + " damage");
+											System.out.println(PokemonVersion.dictionary.get(randomNumber1).getName() + " has " + wildPokemonHP + " HP left");
+											System.out.println();
+											
+											
+										}
+									case 10:
+										{
+
+											totalDamageToEnemy = differenceOfAttAndDef * powerOfSkill / 5;
+											wildPokemonHP = wildPokemonHP - totalDamageToEnemy;
+											System.out.println("Good job, that is critical, you made the " + totalDamageToEnemy + " damage");
+											System.out.println(PokemonVersion.dictionary.get(randomNumber1).getName() + " has " + wildPokemonHP + " HP left");
+											System.out.println();
+											
+										}
+										
 								}
 								
+							
+							int randomNumberAttack2 = (int)(Math.random()*10)+1;
+							
+								switch(randomNumberAttack2)
+								{
+									case 1:
+										{
+											totalDamageToPlayer = 0;
+											myPokemonHP = myPokemonHP - totalDamageToPlayer;
+											System.out.println("the wild pokemon missed, so " + PokemonVersion.dictionary.get(randomNumber1).getName() + " made " + totalDamageToPlayer + " damage to you");
+											System.out.println("your pokemon has " + myPokemonHP + " HP left");
+											System.out.println();
+											
+										}
+									case 2:
+									case 3:
+									case 4:
+									case 5:
+									case 6:
+									case 7:
+									case 8:
+									case 9:
+										{
+											
+											totalDamageToPlayer = differenceOfDefAndAtt * powerOfSkillE / 10;
+											myPokemonHP = myPokemonHP - totalDamageToPlayer;
+											System.out.println(PokemonVersion.dictionary.get(randomNumber1).getName() + " made " + totalDamageToPlayer + " damage to you");
+											System.out.println("your pokemon has " + myPokemonHP + " HP left");
+											System.out.println();
+											
+										}
+									case 10:
+										{
+											totalDamageToPlayer = differenceOfDefAndAtt * powerOfSkillE / 5;
+											myPokemonHP = myPokemonHP - totalDamageToPlayer;
+											System.out.println("Oh no, the wild pokemon made a critical, so " + PokemonVersion.dictionary.get(randomNumber1).getName() + " made " + totalDamageToPlayer + " damage to you");
+											System.out.println("your pokemon has " + myPokemonHP + " HP left");
+											System.out.println();
+											
+										}	
+								}	
+								
+							
+								
+							
+								
+							
+								
+							
 						}
 						
 					}
+						
+						
+						
+					
 				else if(myPokemon.get(0).getLevel()>17&&myPokemon.get(0).getLevel()<37)
 					{
 						
@@ -190,6 +272,47 @@ public class PokemonGame
 						
 					}
 				
+			}
+
+		private static void enemyRandomChooseSkill() 
+			{
+				int randomNumber = 0;
+				if(myPokemon.get(0).getLevel()<17)
+				{
+					randomNumber = (int)(Math.random()*2)+1;
+				}
+				else if(myPokemon.get(0).getLevel()>16&&myPokemon.get(0).getLevel()<37)
+				{
+					randomNumber = (int)(Math.random()*3)+1;
+				}
+				else if(myPokemon.get(0).getLevel()>37)
+				{
+					randomNumber = (int)(Math.random()*4)+1;
+				}
+				else
+				{
+					
+				}
+				switch(randomNumber)
+				{
+					case 1:
+						{
+							powerOfSkillE = PokemonVersion.dictionary.get(2).getSkill1Power();
+						}
+					case 2:
+						{
+							powerOfSkillE = PokemonVersion.dictionary.get(2).getSkill2Power();
+						}
+					case 3:
+						{
+							powerOfSkillE = PokemonVersion.dictionary.get(2).getSkill3Power();
+						}
+					case 4:
+						{
+							powerOfSkillE = PokemonVersion.dictionary.get(2).getSkill4Power();
+						}
+				}
+							
 			}
 
 		private static void userChooseSkill()
@@ -212,22 +335,15 @@ public class PokemonGame
 					{
 						powerOfSkill = myPokemon.get(0).getSkill4Power();
 					}
-				else 
+				else
 					{
-						System.out.println("error choose, please do again");
+						System.out.println("error pick, do it again");
 						userChooseSkill();
+						
 					}
 				
+				
 			}
-
-
-
-
-
-
-
-
-
 
 		private static void exAdding()
 			{
